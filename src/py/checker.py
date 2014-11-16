8e11ba36b7276a907da4c09aef806389f7ec2a83
avlonger <alexander.loptev@gmail.com>
2014-11-16 13:39:02 +0300

    experimentation for longest borderless subwords for alphabets sizes 2, 3, 4

--- src/py/checker.py
+++ src/py/checker.py
@@ -4,12 +4,14 @@
 import string
 import subprocess
 from random import choice
+from itertools import product
+
 
 import pylab as pl
 import numpy as np
 
-EXPERIMENTS = 10000
-LENGTH = 1000
+EXPERIMENTS = 1000
+LENGTH = 100
 AVAILABLE_LETTERS = string.lowercase + string.uppercase + string.digits
 
 
@@ -43,28 +45,62 @@ def is_lyndon_decomposition(word, factor_positions):
 
     return True
 
+
+def is_longest_borderless(text, length, answer):
+    n = len(text)
+    max_len = 1
+    for i in xrange(n):
+        for j in xrange(i + 1, n + 1):
+            subword = text[i:j]
+            for k in xrange(1, len(subword) - 1):
+                subsub = subword[:k]
+                if subword.endswith(subsub):
+                    break
+            else:
+                max_len = max(len(subword), max_len)
+    return max_len == length
+
+
+def all_words(alphabet, length):
+    for result in product(alphabet, repeat=length):
+        yield ''.join(result)
+
 if __name__ == '__main__':
-    for alphabet_size in xrange(50, 51):
+
+    # print 'Testing borderless algorithm in series of {} experiments...'.format(EXPERIMENTS)
+    # for _ in xrange(EXPERIMENTS):
+    #     print _
+    #     text = random_string(LENGTH, len(AVAILABLE_LETTERS))
+    #     process = subprocess.Popen(['../../bin/borderless', text], stdout=subprocess.PIPE)
+    #     output = process.stdout.read().strip()
+    #     length = int(output.splitlines()[0].split()[-1].strip())
+    #     subword = output.splitlines()[1].strip()
+    #     assert is_longest_borderless(text, length, subword)
+    #     print length
+    # print 'OK'
+
+    for alphabet_size in xrange(2, 5):
         average_values = []
-        print 'Plotting for alphabet size: {}'.format(alphabet_size)
-        for length in xrange(1, LENGTH + 1):
-            print 'Length:', length
+        lengths = range(2, 9)
+        for length in lengths:
+            print 'Alphabet:', alphabet_size, 'Length:', length
             values = []
-            for _ in xrange(EXPERIMENTS):
-                print 'Exp:', _
-                text = random_string(length, alphabet_size)
-                process = subprocess.Popen(['../../bin/duval', text], stdout=subprocess.PIPE)
+            for i, text in enumerate(all_words(AVAILABLE_LETTERS[:alphabet_size], length)):
+                if i % 1000 == 0:
+                    print i
+                process = subprocess.Popen(['../../bin/borderless', text], stdout=subprocess.PIPE)
                 output = process.stdout.read().strip()
                 words_count = int(output.splitlines()[0].split()[-1].strip())
                 values.append(words_count)
             average_values.append(np.average(values))
-        pl.plot(xrange(LENGTH), average_values)
-        pl.title('Alphabet size = {}'.format(alphabet_size))
-        pl.xlabel('Text length')
-        pl.ylabel('Factors count')
-        pl.savefig('../../results/py-alphabet-{}.png'.format(alphabet_size))
-        pl.clf()
-
+        print 'OK'
+        pl.plot(lengths, average_values, marker='.', label='$\sigma = {}$'.format(alphabet_size))
+    pl.xlabel('Text length')
+    pl.ylabel('Longest borderless subword')
+    pl.grid()
+    pl.legend(loc=4)
+    pl.savefig('../../results/borderless-alphabets.png')
+    pl.clf()
 
     # print 'Testing duval algorithm in series of {} experiments...'.format(EXPERIMENTS)
     # for _ in xrange(EXPERIMENTS):
