a00a34bdb357c82f2e85a237c3ee21fad858d436
avlonger <alexander.loptev@gmail.com>
2014-12-07 03:29:00 +0300

    some interface for python experiments

--- src/py/experiment.py
+++ src/py/experiment.py
@@ -2,25 +2,47 @@
 """
 """
 import csv
+import argparse
+import datetime
 import subprocess
+import multiprocessing as mp
 from multiprocessing.pool import ThreadPool
 
 
 def worker(args):
-    size, length = args
-    p = subprocess.Popen(['../../bin/experiment', '-s', str(size), '-a', str(length), 'PERIOD_BORDERLESS_DIFF'], stdout=subprocess.PIPE)
+    size, length, algo, binary = args
+    p = subprocess.Popen([binary, '-s', str(size), '-a', str(length), algo], stdout=subprocess.PIPE)
     p.wait()
     return size, length, float(p.stdout.read())
 
 if __name__ == '__main__':
-    ALPHABET_SIZE = 2
-    MAX_LENGTH = 30
-    pool = ThreadPool(5)
-    tasks = [(ALPHABET_SIZE, length) for length in xrange(1, MAX_LENGTH + 1)]
+    parser = argparse.ArgumentParser(description='Calculate some statistics')
+    parser.add_argument('-a', '--alphabets', nargs='*', type=int,
+                        help='Alphabet sizes for which calculate statistics')
+    parser.add_argument('-l', '--lengths', nargs='*', type=int,
+                        help='Word lengths for which calculate statistics')
+    parser.add_argument('-c', '--cpu_count', type=int,
+                        help='Processor cores to use (default: all available)',
+                        default=mp.cpu_count())
+    parser.add_argument('-f', '--filename', help='Output file')
+    parser.add_argument('-b', '--binary', help='Binary to use', default='../../bin/experiment')
+    parser.add_argument('algo', metavar='ALGORITHM',
+                        choices=('MINPERIOD', 'MAXBORDERLESS', 'PERIOD_BORDERLESS_DIFF'))
+    options = parser.parse_args()
 
-    with open('minimal_period.txt', 'w') as out_file:
+    pool = ThreadPool(options.cpu_count)
+
+    tasks = []
+    for alphabet_size in options.alphabets:
+        for length in options.lengths:
+            tasks.append((alphabet_size, length, options.algo, options.binary))
+
+    if options.filename is None:
+        options.filename = '{}-{}.txt'.format(options.algo, datetime.datetime.now().strftime('%Y-%m-%d-%H%M%S'))
+
+    with open(options.filename, 'w') as out_file:
         writer = csv.writer(out_file, delimiter='\t')
-        writer.writerow(('ALPHABET', 'LENGTH', 'MINPERIOD'))
+        writer.writerow(('ALPHABET', 'LENGTH', options.algo))
         for size, length, result in pool.imap_unordered(worker, tasks):
-            writer.writerow((size, length, result))
+            writer.writerow((size, length, '{:.10f}'.format(result)))
             out_file.flush()
