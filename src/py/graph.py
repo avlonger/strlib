71d43b028dcf983570b747374e993082133b8946
avlonger <alexander.loptev@gmail.com>
2015-01-23 05:23:38 +0300

    more data

--- src/py/graph.py
+++ src/py/graph.py
@@ -21,16 +21,16 @@ if __name__ == '__main__':
             counts[alphabet_size][length] += diff
 
         print 'q', ' ', ' '.join(map(lambda x: str(x).rjust(6), counts[2]))
-        for alphabet_size in [3, 4, 5]:
+        for alphabet_size in [2]:
             lengths = sorted(counts[alphabet_size])
             values = map(lambda x: counts[alphabet_size].get(x) * 1.0 / alphabet_size ** x, lengths)
             print alphabet_size, ' ', ' '.join(map(lambda x: '{:.3f}'.format(x).rjust(6), values))
             pl.plot(lengths, values, label='$\sigma = {}$'.format(alphabet_size))
-            pl.axes().set_xlim(2, 17)
+            pl.axes().set_xlim(2, 35)
         pl.legend(loc=2)
         pl.grid()
         pl.xlabel('String length')
-        pl.savefig('length_minus_min_period_3_4_5.png')
+        pl.savefig('length_minus_min_period_2.png')
 
     # with open('../../result_alphabets_borderless.txt') as fd:
     #     reader = csv.reader(fd, delimiter=' ')
